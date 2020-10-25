06506ce7a810406c11e051fe2b534161184890d7
Varoon Gupta <varoongupta99@gmail.com>
2020-10-25 19:26:38 -0400

    random gen ID

--- server/utils.js
+++ server/utils.js
@@ -1,6 +1,9 @@
 const generateRoomId = () => {
-    stubbedRoomId = 'ABCD'
-    return stubbedRoomId
+    //stubbedRoomId = 'ABCD' commented out just for now
+    var uniqid =require('uniqid');
+
+    randRoomId = uniqid.time();
+    return randRoomId
 }
 
 module.exports = {generateRoomId}
