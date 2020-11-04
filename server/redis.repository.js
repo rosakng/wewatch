0676a153685504223feb59deb569d878fb398565
Bill Sheng <billxsheng@gmail.com>
2020-11-04 14:29:03 -0500

    Implemented Redis operations and added sample use cases.

--- server/redis.repository.js
+++ server/redis.repository.js
@@ -1,29 +1,69 @@
 const redis = require("redis");
+const { promisify } = require("util");
 const client = redis.createClient();
+const hgetAsync = promisify(client.hget).bind(client);
+
 
 // on host start
 const initializeRoom = (roomId, numGuests, movies) => {
-    // create room key in Redis
-    // add total number of users in room
-    // add movies (room:movieId = 0)
-    // set total swipes to 0
+    let roomArgs = ["numUsers", numGuests, "total_swipes", 0];
+    movies.forEach(element => {
+        roomArgs.push(element.id);
+        roomArgs.push(0);
+    });
+
+    client.hset(roomId, roomArgs, (err, res) => {
+        if(err) {
+            console.log("ERROR initializeRoom() " + err);
+            return false;
+        } else {
+            return true;
+        }
+    });
 }
 
+// initializeRoom('abc', 7, [{id: 1}, {id: 2}]);
+
 // on user likes movie
-const likeEvent = (movie) => {
-    // increment counter for movie (INCR room:movieId)
-    // increment total swipes
+const likeEvent = (roomId, movie) => {
+    client.hincrby(roomId, movie, 1, (err, res) => {
+        if(err) {
+            console.log("ERROR likeEvent() " + err);
+        } 
+    });
+
+    client.hincrby(roomId, "total_swipes", 1, (err, res) => {
+        if(err) {
+            console.log("ERROR likeEvent() " + err);
+            return false;
+        } 
+    });
+
     // if match ? return movie id
 }
 
+// likeEvent('abc', 1);
+
 // on user dislikes movie
-const dislikeEvent = (movie) => {
-    // increment total swipes
-    // decrement counter (for next sprint)
+const dislikeEvent = (roomId) => {
+    client.hincrby(roomId, "total_swipes", 1, (err, res) => {
+        if(err) {
+            console.log("ERROR dislikeEvent() " + err);
+            return false;
+        } else {
+            return true;
+        }
+    });
 }
 
-const checkCompleted = () => {
-    // return total_swipes
+// dislikeEvent('abc', 1);
+const getTotal = async (roomId) => {
+    const count = await hgetAsync(roomId, "total_swipes");
+    return count;
 }
 
-module.exports = { initializeRoom, likeEvent, dislikeEvent, checkCompleted };
+// getTotal('abc').then((count) => {
+//     console.log(count);
+// });
+
+module.exports = { initializeRoom, likeEvent, dislikeEvent, getTotal };
