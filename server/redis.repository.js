316fabb36da8c143fa0924c56cd6d354e0cdf1a5
Bill Sheng <billxsheng@gmail.com>
2020-11-04 11:58:41 -0500

    Add pseudocode to Redis methods.

--- server/redis.repository.js
+++ server/redis.repository.js
@@ -1,11 +1,31 @@
 const redis = require("redis");
 const client = redis.createClient();
 
-const addRoom = () => {
 
+// on create room
+const addRoomAndHost = (roomId, hostName) => {
+    // create room key in Redis
+    // set participant count to 1
 }
 
-const addGuest = () => {
+// on guest joining room
+const addGuestToRoom = (roomId, guestName) => {
+    // increment participant count
+}
 
+// on get room data
+const getRoom = (roomId) => {
+    // return room data 
+}
+
+// on user dislikes movie
+const dislikeEvent = () => {
+    // decrement counter (for next sprint)
+}
+
+//on user likes movie
+const likeEvent = () => {
+    // increment counter for movie?
+    // if match ? return movie id : return null or empty object
 }
 
