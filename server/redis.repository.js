b77f2bddebdd19bf70d46d4c72911882ad1db5f3
Bill Sheng <billxsheng@gmail.com>
2020-11-04 13:01:45 -0500

    Add checkCompleted method

--- server/redis.repository.js
+++ server/redis.repository.js
@@ -1,24 +1,29 @@
 const redis = require("redis");
 const client = redis.createClient();
 
-
+// on host start
 const initializeRoom = (roomId, numGuests, movies) => {
-    // when host starts
-        // create room key in Redis
-        // add total number of users in room
-        // add movies (room:movieId = 0)
+    // create room key in Redis
+    // add total number of users in room
+    // add movies (room:movieId = 0)
+    // set total swipes to 0
 }
 
-//on user likes movie
-const likeEvent = () => {
-    // on user likes movie
-        // increment counter for movie (INCR room:movieId)
-        // if match ? return movie id : return null or empty object
+// on user likes movie
+const likeEvent = (movie) => {
+    // increment counter for movie (INCR room:movieId)
+    // increment total swipes
+    // if match ? return movie id
 }
 
 // on user dislikes movie
-const dislikeEvent = () => {
+const dislikeEvent = (movie) => {
+    // increment total swipes
     // decrement counter (for next sprint)
 }
 
-module.exports = { initializeRoom, likeEvent};
+const checkCompleted = () => {
+    // return total_swipes
+}
+
+module.exports = { initializeRoom, likeEvent, dislikeEvent, checkCompleted };
