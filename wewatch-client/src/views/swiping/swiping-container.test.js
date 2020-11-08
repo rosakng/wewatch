d160340cdda8fddf91791470d0f45512623e87d1
Rosa Kang <rosakang12@gmail.com>
2020-11-08 16:33:48 -0500

    swiping container test

--- wewatch-client/src/views/swiping/swiping-container.test.js
+++ wewatch-client/src/views/swiping/swiping-container.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import SwipingContainer from './swiping-container';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 
-it('renders the Swiping container', () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Router><SwipingContainer/></Router>, div)
+import MovieDetail from 'views/swiping/movie-detail.js'; 
+
+import { test_movie_data } from 'views/swiping/swiping-data';
+Enzyme.configure({ adapter: new Adapter() });
+
+const location= {
+  state: {
+    roomId: 'roomId',
+    topTenMovies: test_movie_data,
+  }
+}
+describe('Render Swiping Container', () => {
+  const wrapper = shallow(
+    <SwipingContainer location={location} />
+  );
+  it('Renders the movie details component', () => {
+    expect(wrapper.containsMatchingElement(<MovieDetail />)).toBe(true);
+    expect(wrapper.find(MovieDetail).props().title).toBe('The Short Game');
+    const like_button = wrapper.find(InsertEmoticonIcon);
+    like_button.simulate('click');
+    // expect(like_button.props().onClick()).toHaveBeenCalled()
+    // expect(wrapper.find(MovieDetail).debug()).toBe('');
+    // expect(wrapper.find(MovieDetail).props().title).toBe('The Short Game');
+  });
 });
