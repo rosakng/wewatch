7d4d6cf7b522a7c93daca2b164d09f182038c5c9
Varoon Gupta <varoongupta99@gmail.com>
2020-11-22 19:48:05 -0500

    End Session button

--- wewatch-client/src/views/swiping/swiping-container.js
+++ wewatch-client/src/views/swiping/swiping-container.js
@@ -3,43 +3,70 @@ import CloseIcon from '@material-ui/icons/Close';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { Container, Row, Col } from 'reactstrap';
 import { Redirect } from "react-router-dom";
+import queryString from 'query-string';
 
 import socket from 'Socket';
 
 import theme from 'styles/theme';
 import StyledDiv from 'styles/styled-div';
-import Layout from 'views/layout';
-import Tooltip from 'views/tooltip';
-import ToolTipIcon from 'views/assets/tooltipIcon';
 import MovieDetail from 'views/swiping/movie-detail.js';
 
 const SwipingContainer = (props) => {  
-  const [noMatch, setNoMatch] = useState(false);
-  const [match, setMatch] = useState(false);
-  const [matchedMovie, setMatchedMovie] = useState({});
-  const [SwipingCompleted, setSwipingCompleted] = useState(false);
+    const [tryAgain, setTryAgain] = useState(false);
+    const [tryAgainHost, setTryAgainHost] = useState(false);
+    const [newRoomId, setNewRoomId] = useState('');
+      
+    const [noMatch, setNoMatch] = useState(false);
+    const [match, setMatch] = useState(false);
+    const [matchedMovie, setMatchedMovie] = useState({});
+    const [SwipingCompleted, setSwipingCompleted] = useState(false);
 
-  const topTenMovies = props.location.state.topTenMovies;
-  const roomId = props.location.state.roomId;
-  const isHost = props.location.state.isHost;
-  const name = props.location.state.name;
+    const topTenMovies = props.location.state.topTenMovies;
+    const roomId = props.location.state.roomId;
+    const isHost = props.location.state.isHost;
+    const name = props.location.state.name;
 
-  const [index, setIndex]  = useState(0);
-  const [title, setTitle] = useState(topTenMovies[index].title);
-  const [imageURL, setImageUrl] = useState(topTenMovies[index].image);
-  const [year, setYear] = useState(topTenMovies[index].released);
-  const [lengthOfMovie, setLengthOfMovie] = useState(topTenMovies[index].duration);
-  const [rating, setRating] = useState(topTenMovies[index].rating);
-  const [mediaType, setmediaType] = useState(topTenMovies[index].type);
-  const [description, setDescription] = useState(topTenMovies[index].synopsis);
+    const [index, setIndex]  = useState(0);
+    const [title, setTitle] = useState(topTenMovies[index].title);
+    const [imageURL, setImageUrl] = useState(topTenMovies[index].image);
+    const [year, setYear] = useState(topTenMovies[index].released);
+    const [lengthOfMovie, setLengthOfMovie] = useState(topTenMovies[index].duration);
+    const [rating, setRating] = useState(topTenMovies[index].rating);
+    const [mediaType, setmediaType] = useState(topTenMovies[index].type);
+    const [description, setDescription] = useState(topTenMovies[index].synopsis);
 
-  const iterateMovie = (index) => {
-    if(index !== (topTenMovies.length - 1)) {
-      setIndex(index + 1);
-    } else {
-      setSwipingCompleted(true)
+    const iterateMovie = (index) => {
+      if(index !== (topTenMovies.length - 1)) {
+        setIndex(index + 1);
+      } else {
+        setSwipingCompleted(true)
+      }
+    };
+
+    const tryAgainEmit = () => {
+      //emit try again event
+      socket.emit('try_again_event', {roomId: roomId})
+
     }
-  };
+
+    //listen to try again event
+    useEffect(() => {
+      socket.on('tryAgainRedirectHost', () => {
+          if (isHost) {
+              setTryAgainHost(true);
+          } 
+      })
+      return () => { socket.off('tryAgainRedirectHost')};
+    });
+
+    //listen to try again event for users
+    useEffect(() => { 
+      socket.on('tryAgainRedirectUser', ({newRoomId}) => {
+        setNewRoomId(newRoomId)
+        setTryAgain(true);
+      });
+    return () => { socket.off('tryAgainRedirectUser')};
+    })
 
   useEffect(() => {
     setTitle(topTenMovies[index].title);
@@ -68,90 +95,72 @@ const SwipingContainer = (props) => {
         setNoMatch(true);
    });
 
-   return () => { socket.off('noMatchRedirect')};
+  //listen to match event
+    socket.on('matchRedirect', ({matchedMovieId, matchedMovieData}) => {
+      console.log(matchedMovieId);
+      console.log(matchedMovieData);
+      setMatchedMovie(matchedMovieData);
+      setMatch(true);
+    });
   });
 
-  useEffect(() => {
-      //listen to match event
-      socket.on('matchRedirect', ({matchedMovieId, matchedMovieData}) => {
-        setMatchedMovie(matchedMovieData);
-        setMatch(true);
-      });
-  
-      return () => { socket.off('matchRedirect')};
-  })
-
   function SwipingCompletedScreen () {
-    return(
-      <Layout>
-        <Container>{ match && matchedMovie != null ? <Redirect to={{ 
+    return(<Container>{ match && matchedMovie != null ? <Redirect to={{ 
       pathname: '/match',
       state: {matchedMovie: matchedMovie}
       }}/>: null }
-      { noMatch ? <Redirect to={{
-        pathname: '/noMatch',
-        state: {isHost: isHost,
-                roomId: roomId,
-                name: name}
-      }}/> : null }<h1 style={{'text-align': "center", 'margin-top': '60px'}}>You've seen all potential movies for recommendation, please wait as your the others finish swiping!</h1>
-        </Container>
-      </Layout>
-      
-    )
+      { noMatch ? <Redirect to='/noMatch'/> : null }<h1 style={{'text-align': "center", 'margin-top': '60px'}}>You've seen all potential movies for recommendation, please wait as your the others finish swiping!</h1></Container>)
   }
 
   if (!SwipingCompleted){
   return (
-    <Layout>
-      <StyledDiv flex paddingLeft={4} marginTop={3}>
-          <StyledDiv>Start Swiping!</StyledDiv>
-          <Tooltip
-            text={`Click the happy face if you would like to watch the movie, or click the X otherwise!`}
-          >
-            <ToolTipIcon fill={theme.colors.gray[2]} size="25px" />
-          </Tooltip>
-        </StyledDiv>
-      <Container>
-            { match && matchedMovie != null ? <Redirect to={{ 
+  <Container>
+    { match && matchedMovie != null ? <Redirect to={{ 
                                 pathname: '/match',
                                 state: {matchedMovie: matchedMovie}
                             }}/>: null }
-    { noMatch ? <Redirect to={{
-                                pathname: '/noMatch',
-                                state: {isHost: isHost,
-                                        roomId: roomId,
-                                        name: name}
-                              }}/> : null }
-          <Row>
-            <Col>
-            {props.location.state.topTenMovies && (
-              <StyledDiv flex alignItems="center" marginTop={2}>
-                <CloseIcon
-                  style={{ color: theme.colors.red, fontSize: '60px'}}
-                  onClick={onClickDislike}
-                />
-                <StyledDiv padding={2}>
-                  <MovieDetail
-                    title={title}
-                    year={year}
-                    lengthOfMovie={lengthOfMovie}
-                    rating={rating}
-                    mediaType={mediaType}
-                    imageURL={imageURL}
-                    description={description}
-                  />
-                </StyledDiv>
-                <InsertEmoticonIcon
-                  style={{ color: theme.colors.green, fontSize: '60px'}}
-                  fontSize="large"
-                  onClick={onClickLike}
-                />
-              </StyledDiv>
-            )}
-            </Col>
-          </Row>
-        </Container>
-    </Layout>
+    { noMatch ? <Redirect to='/noMatch'/> : null }
+    <Row>
+      <Col>
+      {props.location.state.topTenMovies && (
+        <StyledDiv flex alignItems="center" marginTop={2}>
+          <CloseIcon
+            style={{ color: theme.colors.red, fontSize: '60px'}}
+            onClick={onClickDislike}
+          />
+          <StyledDiv padding={2}>
+            <MovieDetail
+              title={title}
+              year={year}
+              lengthOfMovie={lengthOfMovie}
+              rating={rating}
+              mediaType={mediaType}
+              imageURL={imageURL}
+              description={description}
+            />
+            <StyledDiv>
+            <div className="buttonContainer">
+                {isHost ? <button className={'button mt-20'} type="button" onClick={tryAgainEmit}>End Session</button> : null}
+            </div>
+            </StyledDiv>
+          </StyledDiv>
+
+          <InsertEmoticonIcon
+            style={{ color: theme.colors.green, fontSize: '60px'}}
+            fontSize="large"
+            onClick={onClickLike}
+          />
+        </StyledDiv>
+      )}
+      </Col>
+    </Row>
+    {/* {name === hostName 
+        ? 
+        <button className={'button mt-20'} type="button" onClick={}>End Session</button> 
+        :
+        <h2>only the host may end the session</h2> 
+    } */}
+  </Container>
   );}
   else if (SwipingCompleted) {
     return(<SwipingCompletedScreen/>)
